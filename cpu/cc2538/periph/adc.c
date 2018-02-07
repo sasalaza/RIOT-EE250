ab893fed3736efbdaa5f82dda11b7539b56afef5
ee250 <sasalaza@usc.edu>
2018-02-06 19:43:36 -0800

    back to original files

--- cpu/cc2538/periph/adc.c
+++ cpu/cc2538/periph/adc.c
@@ -25,7 +25,7 @@
 #include "periph_conf.h"
 #include "periph_cpu.h"
 #include "periph/adc.h"
-#include "periph/gpio.h"
+*#include "periph/gpio.h"
 
 #define ENABLE_DEBUG (0)
 #include "debug.h"
@@ -44,7 +44,8 @@ int adc_init(adc_t line)
     adca->ADCCON2 = 0x0;
     /* configure ADC GPIO as analog input */
     gpio_init(adc_config[line], GPIO_IN_ANALOG);
-	gpio_init(GPIO_PIN(PORT_D,2), GPIO_OUT);
+	
+//gpio_init(GPIO_PIN(PORT_D,2), GPIO_OUT);
 
 
 
@@ -94,13 +95,13 @@ int adc_sample(adc_t line, adc_res_t res)
     DEBUG("ADCCON1: %"PRIu32" ADCCON2: %"PRIu32" ADCCON3: %"PRIu32"\n",
           adca->cc2538_adc_adccon1.ADCCON1, adca->ADCCON2, adca->ADCCON3);
 
-	gpio_set(GPIO_PIN(PORT_D,2));
+	//gpio_set(GPIO_PIN(PORT_D,2));
 
     /* Poll/wait until end of conversion */
     while ((adca->cc2538_adc_adccon1.ADCCON1 &
             SOC_ADC_ADCCON1_EOC_MASK) == 0) {}
 
-gpio_clear(GPIO_PIN(PORT_D,2)); 
+//gpio_clear(GPIO_PIN(PORT_D,2)); 
 
     /* Read result after conversion completed,
      * reading SOC_ADC_ADCH last will clear SOC_ADC_ADCCON1.EOC */
